dcf632fa9942a6e81118fc63561bf567dbe8e906
TheAbhishekraj <rajabhi2602@gmail.com>
2023-07-02 23:40:51 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="theabhishekraj",
+    author_email="theabhishekraj@gmail.com",
+    packages=find_packages(),
+    install_requires=['Pandas'],
+)
+
